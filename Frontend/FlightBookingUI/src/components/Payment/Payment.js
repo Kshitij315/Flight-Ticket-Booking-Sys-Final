a685bce90697b7ef52c4aa55c51ca71c37f87f53
Kshitij315 <kshitijkk15@gmail.com>
2024-10-21 17:00:24 +0530

    changes to summary and payment

--- Frontend/FlightBookingUI/src/components/Payment/Payment.js
+++ Frontend/FlightBookingUI/src/components/Payment/Payment.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import BookingService from '../../services/BookingService';
 import { FaCcAmex, FaCcMastercard, FaCcVisa } from 'react-icons/fa6';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 import Swal from 'sweetalert2';
 import './Payment.css'
+import mastercard from  '../../assets/mastercardlogo.png'
+import visa from '../../assets/visalogo.png'
+import creditcard from '../../assets/creditcard.png'
+import { BiFontSize } from 'react-icons/bi';
+
+
+
 
 export default function Payment() {
     const [ticketNumber, setticketNumber] = useState(0);
@@ -33,21 +40,22 @@ export default function Payment() {
     };
     return (
         <div className='home'>
+            <div className='payment-card'>
             <div className="payment-container">
                 <h4>Confirm Payment</h4>
-                <div>
-                    <span><h6>CREDIT/DEBIT CARD</h6></span>
+                <div className='card-icon'>
+                    
+                        <img src={mastercard} width={50} height={10}/> 
+                        <img src={visa}/>
+                        <img src={creditcard}/>
+
                 </div>
-                <div>
-                    <FaCcMastercard />
-                    <FaCcVisa />
-                    <FaCcAmex />
-                </div>
-            
+               
             <Formik
                 initialValues={{
                     cnumber: '',
-                    exp: '',
+                    exp1: '',
+                    exp2:'',
                     cvc: '',
                     name: ''
                 }}
@@ -79,43 +87,57 @@ export default function Payment() {
             >
                 {({ error, touched }) => (
                     <Form>
+                        <div className='card-details'>
                         <div>
                             <label htmlFor='cc-number'>
                                 CARD NUMBER
                             </label>
-                            <Field name="cnumber" type="text" placeholder="**** **** **** ****" />
+                            <Field name="cnumber" type="text" placeholder="XXXXXXXXXXXXXXXX" />
                             <ErrorMessage name='cnumber' component='div' />
                         </div>
                         <br />
                         <div>
-                            <label htmlFor='cc-exp'>
-                                CARD EXPIRY
-                            </label>
-                            <Field name='exp' type='month' />
-                            <ErrorMessage name='exp' component='div' />
-                        </div>
-                        <div>
-                            <label htmlFor='cc-cvc'>
-                                CARD CVV
-                            </label>
-                            <Field name='cvc' type='text' placeholder='***' />
-                            <ErrorMessage name='cvc' component='div' />
-                        </div>
-                        <br />
-                        <div>
-                            <label htmlFor='holder-name'>
+                        <label htmlFor='holder-name'>
                                 CARD HOLDER NAME
                             </label>
-                            <Field name='name' type='text' placeholder='Enter CardHolder name' />
+                            <Field name='name' type='text' placeholder='Enter Name on the card' />
                             < ErrorMessage name='name' component='div' />
+                            {/* */}
                         </div>
+                        
+                       <br/>
+                       <div className="expiry-security">
+                        <label htmlFor='cc-exp'> EXPIRY DATE </label>
+                        
+                            <div className='expiry'>
+                            <Field name='exp1'  placeholder="MM"  type='month' className='month-input' />
+                            <ErrorMessage name='exp'  placeholder="YYYY" component='div'/>
+                           
+                            <span> /</span>
+                            <Field name='exp2'  placeholder="YY" type='year' className='year-input'/>
+                            <ErrorMessage name='exp' component='div'  />
+                            </div>
+                            <br/>
+                            <label htmlFor='cc-cvc'> CARD CVV </label>
+                            <Field name='cvc' type='password' placeholder='***' className='security'/>
+                            <ErrorMessage name='cvc' component='div'/>
+                            
+                            </div>
+                            {/*  /> */}
+                        
                         <br />
-                        <div>
-                            <button type='submit'>Make Payment</button>
+            
+                        </div>
+                        
+                        <div className='card-footer'>
+                            {/* <Link to='/ticket'> */}
+                            <button type='submit' className='subscribe btn btn-primary btn-block shadow-sm'><b>Pay Now</b></button>
+                            {/* </Link> */}
                         </div>
                     </Form>
                 )}
             </Formik>
+        </div>
         </div>
         </div>
 
